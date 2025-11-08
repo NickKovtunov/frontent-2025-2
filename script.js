146e41902a8c8befa79e0bc3a7995733442cac39
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2025-11-08 15:52:17 +0500

    ls-8

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal(){
+    document.getElementById("modalWrap").style.display = "flex";
+    document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal(){
+    document.getElementById("modalWrap").style.display = "none";
+    document.getElementById("modal").style.display = "none";
+}
